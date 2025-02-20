92609d45a25fa05a55a30605fadc81384972368e
Aaron Kammer <aaron.jordan.kammer@uni-weimar.de>
2025-02-20 17:02:20 +0100

    dirty work around flipped normals

--- depth_pruning/depth2normals.py
+++ depth_pruning/depth2normals.py
@@ -18,7 +18,7 @@ def get_camera_rays(H, W, focal):
 
 
 # https://atyuwen.github.io/posts/normal-reconstruction
-def compute_normals(depths, points, view_dirs):
+def compute_normals(depths, points):
     # get 2 pixel depths neighbors in each direction
     neighbor_depths = [
         torch.roll(depths, shifts=(-1, 0), dims=(0, 1)),
@@ -33,63 +33,56 @@ def compute_normals(depths, points, view_dirs):
 
     neighbors = [
         torch.roll(points, shifts=(-1, 0), dims=(0, 1)),
-        torch.roll(points, shifts=(-2, 0), dims=(0, 1)),
         torch.roll(points, shifts=(1, 0), dims=(0, 1)),
-        torch.roll(points, shifts=(2, 0), dims=(0, 1)),
         torch.roll(points, shifts=(0, -1), dims=(0, 1)),
-        torch.roll(points, shifts=(0, -2), dims=(0, 1)),
         torch.roll(points, shifts=(0, 1), dims=(0, 1)),
-        torch.roll(points, shifts=(0, 2), dims=(0, 1)),
     ]
 
-    # make extrapolation of center depth from each pair of neighbors
-    leftExtrap = torch.abs(neighbor_depths[0] - neighbor_depths[1])
-    rightExtrap = torch.abs(neighbor_depths[2] - neighbor_depths[3])
-    upExtrap = torch.abs(neighbor_depths[4] - neighbor_depths[5])
-    downExtrap = torch.abs(neighbor_depths[6] - neighbor_depths[7])
+    # make a re-estimation of the center depth by extrapolating from neighbors
+    # get difference between extrapolation and original depth
+    extrap_l = torch.abs(2 * neighbor_depths[0] - neighbor_depths[1] - depths)
+    extrap_r = torch.abs(2 * neighbor_depths[2] - neighbor_depths[3] - depths)
+    extrap_u = torch.abs(2 * neighbor_depths[4] - neighbor_depths[5] - depths)
+    extrap_d = torch.abs(2 * neighbor_depths[6] - neighbor_depths[7] - depths)
 
     # mask which extrapolation is more accurate to original depth (left vs right, up vs down)
-    horzMask = leftExtrap < rightExtrap
-    vertMask = upExtrap < downExtrap
+    maks_horz = extrap_l < extrap_r
+    mask_vert = extrap_u < extrap_d
 
     # broadcast masks to the same shape as neighbors
-    horzMask = horzMask.unsqueeze(-1).expand(-1, -1, 3)
-    vertMask = vertMask.unsqueeze(-1).expand(-1, -1, 3)
+    maks_horz = maks_horz.unsqueeze(-1).expand(-1, -1, 3)
+    mask_vert = mask_vert.unsqueeze(-1).expand(-1, -1, 3)
 
     # choose more accurate extrapolation for each direction
-    horzDeriv = torch.where(horzMask, neighbors[0] - neighbors[1], neighbors[2] - neighbors[3])
-    vertDeriv = torch.where(vertMask, neighbors[4] - neighbors[5], neighbors[6] - neighbors[7])
+    deriv_horz = torch.where(maks_horz, points - neighbors[0], neighbors[1] - points)
+    deriv_vert = torch.where(mask_vert, points - neighbors[2], neighbors[3] - points)
 
-    # create normalized normals from cross product of derivatives
-    normal = torch.cross(vertDeriv, horzDeriv)
+    # create normals from cross product of derivatives
+    normal = torch.cross(deriv_vert, deriv_horz)
     normal = F.normalize(normal, dim=-1)
-
-    # account for different cross product directions, flip normals
-    normal[~horzMask] *= -1
-    normal[~vertMask] *= -1
     return normal
 
 
 def get_normal_map(depth_map, key, cameras, images: List[rwm.Image]):
-    rgba = cv2.imread("/home/mighty/repos/datasets/hah/esszimmer_small/test_depth.png", cv2.IMREAD_UNCHANGED)
-    inv_depth_map = rgba.view(np.float32).reshape(rgba.shape[0], rgba.shape[1])
-    eps = 1e-6  # Small value to avoid division by zero
-    depth_map2 = torch.tensor(1.0 / (inv_depth_map + eps))
+    # rgba = cv2.imread("/home/mighty/repos/datasets/hah/esszimmer_small/test_depth.png", cv2.IMREAD_UNCHANGED)
+    # inv_depth_map = rgba.view(np.float32).reshape(rgba.shape[0], rgba.shape[1])
+    # eps = 1e-6  # Small value to avoid division by zero
+    # depth_map = torch.tensor(1.0 / (inv_depth_map + eps))
 
-    invmonodepthmap = cv2.imread("/home/mighty/repos/datasets/tandt/train/depths/00241.png", cv2.IMREAD_UNCHANGED)
-    invmonodepthmap = invmonodepthmap.astype(np.float32) / (2**16)
-    if invmonodepthmap.ndim != 2:
-        invmonodepthmap = invmonodepthmap[..., 0]
-    invmonodepthmap[invmonodepthmap < 1e-3] = np.nan
-    depth_map =  torch.tensor(1.0 / (invmonodepthmap + 1e-6))
-    # breakpoint()
+    # invmonodepthmap = cv2.imread("/home/mighty/repos/datasets/hah/esszimmer_small/depth_any/DSC00087_DxO_83.png", cv2.IMREAD_UNCHANGED)
+    # if invmonodepthmap.ndim != 2:
+    #     invmonodepthmap = invmonodepthmap[..., 0]
+    # invmonodepthmap = invmonodepthmap.astype(np.float32) / (2**16)
+    # invmonodepthmap[invmonodepthmap < 1e-3] = np.nan
+    # depth_map =  torch.tensor(1.0 / (invmonodepthmap + 1e-6))
 
+    # i dont know flip and reflip
+    depth_map = depth_map.fliplr()
 
     image_meta = images[key]
     cam_intrinsic = cameras[image_meta.camera_id]
     depth_shape = depth_map.shape
     map_scale = depth_shape[0] / cam_intrinsic.height
-    map_scale = 1
 
     f_x = cam_intrinsic.params[0]
     dirs = get_camera_rays(depth_shape[0], depth_shape[1], f_x * map_scale)
@@ -105,26 +98,30 @@ def get_normal_map(depth_map, key, cameras, images: List[rwm.Image]):
     # dr.storePly(ply_path, cloud.points, cloud.colors)
     # ---
 
-    normal_map = compute_normals(depth_map, points, dirs)
-    return normal_map.permute(1, 2, 0)  # Convert back to (H, W, 3)
+    normal_map = compute_normals(depth_map, points)
+    return normal_map.fliplr()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_dir', default="../datasets/hah/esszimmer_small")
-    parser.add_argument('--depths_dir', default="../datasets/hah/esszimmer_small/depth")
+    parser.add_argument('--depths_dir', default="../datasets/hah/esszimmer_small/depth_32")
     parser.add_argument('--model_type', default="bin")
     args = parser.parse_args()
 
     cam_intrinsics, images_metas, points3d = rwm.read_model(os.path.join(args.base_dir, "sparse", "0"), ext=f".{args.model_type}")
     key = 83
-    inv_depth_map = torch.tensor(cv2.imread("/home/mighty/repos/datasets/hah/esszimmer_small/depth/DSC00087_DxO_83.png", -1).astype(np.float32) / 512)
-    depth_map = 1.0 / (inv_depth_map + 1e-6)
+    # rgba = cv2.imread("/home/mighty/repos/datasets/hah/esszimmer_small/depth_32/DSC00087_DxO_83.png", cv2.IMREAD_UNCHANGED)
+    rgba = cv2.imread("/home/mighty/repos/datasets/hah/esszimmer_small/depth_32/DSC00074_DxO_71.png", cv2.IMREAD_UNCHANGED)
+    inv_depth_map = rgba.view(np.float32).reshape(rgba.shape[0], rgba.shape[1])
 
+    eps = 1e-6  # Small value to avoid division by zero
+    depth_map = torch.tensor(1.0 / (inv_depth_map + eps))
+    depth_map = depth_map
 
     normal_map = get_normal_map(depth_map, key, cam_intrinsics, images_metas)
-    normal_map = 0.5 * normal_map + 0.5
+    normal_map = 0.5 * (normal_map + 1.0)
 
     plt.figure(figsize=(10, 8))
-    plt.subplots_adjust(left=0, right=1, top=1, bottom=0) # rm window padding
-    plt.imshow(normal_map.permute(2, 0, 1), cmap='plasma', interpolation='nearest')
+    # plt.subplots_adjust(left=0, right=1, top=1, bottom=0) # rm window padding
+    plt.imshow(normal_map, interpolation='nearest')
     plt.show()
